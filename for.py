8dd124f102a6547765a3f3181ac23436bddf0e74
cBOCANEGRA9625 <bocanegra2596@outlook.com>
2025-05-17 17:17:14 -0600

    se agregan programas de if y for

---
+++ for.py
@@ -0,0 +1,4 @@
+lista = ["gato", "perro", "vaca", "caballo"]
+for item in lista:
+    print ("estos son los animales que has visto:", item )
+
---
+++ if.py
@@ -0,0 +1,19 @@
+# animal = input("dime un animal")
+# if animal == "perro":
+#     print ("-guau")
+# print ("conozco pocos animales")
+
+# num = int(input("ingrese un entero"))
+# if num<0:
+#     num = -num
+# print("su valor absoluto es: ", num)
+
+servicio = int (input ("Que kilometraje tiene tu carro? "))
+
+if servicio < 10000:
+    print ("servicio de cambio de aceite")
+elif servicio > 40000:
+    print ("servicio aceite y cambio de balatas")
+
+
+
