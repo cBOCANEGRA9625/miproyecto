6f6b2ab94f666d91992766dcfa0ba62eda9e99d9
cBOCANEGRA9625 <bocanegra2596@outlook.com>
2025-05-24 12:30:40 -0600

    se actualizacn programas

--- if.py
+++ if.py
@@ -1,19 +1,51 @@
 # animal = input("dime un animal")
 # if animal == "perro":
-#     print ("-guau")
+#      print ("-guau")
 # print ("conozco pocos animales")
 
 # num = int(input("ingrese un entero"))
 # if num<0:
-#     num = -num
+#      num = -num
 # print("su valor absoluto es: ", num)
 
-servicio = int (input ("Que kilometraje tiene tu carro? "))
+# servicio = int (input ("Que kilometraje tiene tu carro? "))
 
-if servicio < 10000:
-    print ("servicio de cambio de aceite")
-elif servicio > 40000:
-    print ("servicio aceite y cambio de balatas")
+# if servicio < 10000:
+#     print ("servicio de cambio de aceite")
+# elif servicio > 40000:
+#     print ("servicio aceite y cambio de balatas")
 
+# Salir = input ("¿Saldras el dia de hoy?" )
 
+# edad = false
 
+# if entrada .isnumeric() :
+#      edad = int(entrada)
+# else :
+#      print ("Dato incorrecto, es necesario un numero")
+#      exit ()
+
+# if edad <= 0:
+#      print ("Eres demaciado joven... Imposible. Trata de nuevo")
+# elif edad > 100 :
+#      print ("Alguien tan viejo, no deberias fumar. TRata de nuevo")
+# elif edad < 18 :
+#      print ("alguien menor de edad no deberia fumar...")
+# else :
+#      print ("eres mayor de edad, aqui esta tu cigarro")
+
+Salir = input ("¿Saldras de casa hoy?: ")
+
+if Salir == "si" :
+    if Salir == "si" : 
+        print ("¿Apagaste todas las luces?: ")
+elif Salir == "si" :
+     print ("¿Las mascotas tienen agua y comida?: ")
+elif Salir == "si":
+   print ("¿La puerta tiene seguro?: ")
+elif Salir == "si":
+   print ("¿Llevas tu cartera?: ")
+else :
+     print ("Tienes todo listo para salir")
+ else:
+    print ("Es mejor estar en casa")
---
+++ while.py
@@ -0,0 +1,6 @@
+factorial = 10
+contador = factorial - 1
+while contador > 0:
+    factorial *= contador
+    contador -= 1
+print (factorial)
